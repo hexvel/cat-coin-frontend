7c9dc0ee31af59c8fa4dd3f250a96c0f91b80554
Hexvel <Hexvel>
2024-07-18 02:22:14 +0300

    fix

--- store/api/user.ts
+++ store/api/user.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const userApi = createApi({
 	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://127.0.0.1:8000/api/v1',
+		baseUrl: 'https://api.meowcatcoinbot.ru/api/v1',
 	}),
 	tagTypes: ['User'],
 	endpoints: builder => ({
